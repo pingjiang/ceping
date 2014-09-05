2f66d543985cbef75cb8c540b21beec718666054
pingjiang <pingjiang1989@gmail.com>
2014-09-05 17:13:34 +0800

    Initial Commit

---
+++ client/app/admin/admin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('cepingApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('admin', {
+        url: '/admin',
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
